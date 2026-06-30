fc1ae0d4e532629476f6290df55185e0e0471508
sofiseferin-09 <sofia.seferin@escola.pr.gov.br>
2026-06-30 11:47:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const botoes=document.querySelectorAll(".botao");
 const textos=document.querySelectorAll(".aba-conteudo");
+for(let i =0;i <botoes.length; i++){ 
+    botoes[i].onclick =function(){
+      
+        for(let i =0; i <botoes.length; j++){ 
+            botoes[j].classList.remover("ativo");
+            textos[j].classList.remover("ativo");
 
-for(let i =0; i <botoes.length; j++){  
-   botoes[j].classList.remover("ativo");
+    }
+botoes[i].classList.add("ativo");
+textos[i].classList
 }
+
